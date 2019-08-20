982a35d10ac8f28be67a64786b193759dc2e3006
JunichiQueen <junichim@hotmail.com>
2019-08-20 11:00:26 +0100

    financeget

--- src/Skyre/Individual.js
+++ src/Skyre/Individual.js
@@ -1,137 +1,176 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import {Tab, Tabs, Modal, Button, ModalBody} from 'react-bootstrap';
+import axios from 'axios';
 
 
 
-export default function Individual (props) {
-    const [show, setShow] = useState(false);
+export default class Individual extends Component {
+    constructor(props){
+        super();
+        this.state={
+            show: false,
+            financeData: [],
+            mobileData: [],
+            vehicleData: []
+        }
+    }
+
+    getFinance = () => {
+        let forenames = "forenames=" + this.props.firstname + "&";
+        let surname = "surname=" + this.props.lastname + "&";
+        // let homeAddress = "homeAddress=" + this.props.address + "&";
+        // let dateOfBirth = "dateOfBirth=" + this.props.dateOfBirth + "&";
+        let appender = "" + forenames + surname;
+        axios
+        .get(`http://localhost:5000/Citizen/getFinance/${appender}`)
+        .then(response => {
+            this.setState({
+                financeData: response.data
+            })
+        })
+        .catch(err => console.log(err))
+    }
+
+    getMobile = () => {
+        axios
+        .get("URL")
+        .then(response => {
+            this.setState({
+                financeData: response.data
+            })
+        })
+        .catch(err => console.log(err))
+    }
+
+    getVehicle = () => {
+        axios
+        .get("URL")
+        .then(response => {
+            this.setState({
+                financeData: response.data
+            })
+        })
+        .catch(err => console.log(err))
+    }
   
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
-    return (
-      <div>
+    handleClose = () => {
+        this.setState({
+            show: false
+        })
+    }
+    handleShow = () => {
+        this.getFinance();
+        this.setState({
+            show: true
+        })
+    }
 
-        
-        <p>{props.firstname + " "}{props.lastname}</p>
-        <p>{props.address}</p>
-        {/* <p>{props.citizenId}</p>   */}
-        
-        
-
-        <Button variant="primary" onClick={handleShow}>
-        More details
-      </Button>
-      {/* <Modal.Title>X's record</Modal.Title> */}
-      <Modal size="lg" 
-            show={show} 
-            onHide={handleClose} 
-            dialogClassName="modal-150w"
-            >
-        <Modal.Header closeButton><Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-    <Tab eventKey="Biography" title="Biography">
-           
-        <Modal.Body class="modal-body">
-            
-                <ModalBody><b>Citizen Id:</b>{" " + props.citizenId}</ModalBody>
+    render(){
+        const {
+            citizenId,
+            firstname,
+            lastname,
+            address,
+            dateOfBirth,
+            placeOfBirth,
+            sex,
+        } = this.props;
+        return (
+            <div>
+      
+                <p>{firstname + " "}{lastname}</p>
+                <p>{address}</p>               
                 
-                <ModalBody><b>Name:</b> {"\n"} {" " + props.firstname + " "}{props.lastname}</ModalBody>
-            
-                <ModalBody><b>Address:</b>{" " + props.address}</ModalBody>
+                <Button variant="primary" onClick={this.handleShow}>
+                More details
+                </Button>
+                <Modal size="lg" 
+                    show={this.state.show} 
+                    onHide={this.handleClose} 
+                    dialogClassName="modal-150w"
+                    >
+                <Modal.Header closeButton>
+                <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+                    <Tab eventKey="Biography" title="Biography">
+                    
+                        <Modal.Body class="modal-body">
+                            
+                                <ModalBody><b>Citizen Id:</b>{" " + citizenId}</ModalBody>
+                                
+                                <ModalBody><b>Name:</b> {"\n"} {" " + firstname + " "}{lastname}</ModalBody>
+                            
+                                <ModalBody><b>Address:</b>{" " + address}</ModalBody>
 
-                <ModalBody><b>Sex:</b>{" " + props.sex}</ModalBody>
+                                <ModalBody><b>Sex:</b>{" " + sex}</ModalBody>
 
-                <ModalBody><b>Place Of Birth:</b>{" " + props.placeOfBirth}</ModalBody>
-            
-                <ModalBody><b>Date Of Birth:</b>{" " + props.dateOfBirth}</ModalBody>
-            
-            
-        </Modal.Body>
+                                <ModalBody><b>Place Of Birth:</b>{" " + placeOfBirth}</ModalBody>
+                            
+                                <ModalBody><b>Date Of Birth:</b>{" " + dateOfBirth}</ModalBody>
+                            
+                            
+                        </Modal.Body>
 
-    
-        
-    </Tab>
-    <Tab eventKey="Financial details" title="Financial details">
-        
-    <Modal.Body class="modal-body2">
-            
-            <ModalBody><b>Bank</b>{" " + props.bank}</ModalBody>
-            
-            <ModalBody><b>Bank card number:</b> {" " + props.bankCardNumber}</ModalBody>
-        
-            <ModalBody><b>Payee account:</b>{" " + props.payeeAccount}</ModalBody>
+                
+                    
+                    </Tab>
+                    <Tab eventKey="Financial Details" title="Financial Details">
+                    
+                        <Modal.Body class="modal-body2">
 
-            <ModalBody><b>Transactions:</b>{" " + props.transactions}</ModalBody>
+                            <ModalBody><b>Bank Account Id:</b>{" " + this.state.financeData.map((item) => item.bankAccountId)}</ModalBody>
 
-            
-        
-    </Modal.Body>
+                            <ModalBody><b>Bank Account Number:</b> {" " + this.state.financeData.map((item => item.accountNumber))}</ModalBody>
+                            
+                            <ModalBody><b>Bank</b>{" " + this.state.financeData.map((item) => item.bank)}</ModalBody>                 
+                            
+                        
+                        </Modal.Body>
 
-    </Tab>
-    <Tab eventKey="ANPR" title="ANPR">
-    
-    <Modal.Body class="modal-body3">
-            
-            <ModalBody><b>ANPR Point Id:</b>{" " + props.anprPointId}</ModalBody>
-            
-            <ModalBody><b>Timestamp:</b> {" " + props.timestamp}</ModalBody>
-        
-            <ModalBody><b>Vehicle Registration number:</b>{" " + props.vehicleRegistrationNumber}</ModalBody>
+                    </Tab>
+                    <Tab eventKey="ANPR" title="ANPR">
+                
+                        <Modal.Body class="modal-body3">
+                            
+                            <ModalBody><b>ANPR Point Id:</b>{" " + this.props.anprPointId}</ModalBody>
+                            
+                            <ModalBody><b>Timestamp:</b> {" " + this.props.timestamp}</ModalBody>
+                        
+                            <ModalBody><b>Vehicle Registration number:</b>{" " + this.props.vehicleRegistrationNumber}</ModalBody>
 
-            
+                        </Modal.Body>
 
-            
-    </Modal.Body>
+                    </Tab>
+                
+                    <Tab eventKey="Associates" title="Associates">
+                
+                        <Modal.Body class="modal-body2">
+                            
+                            <ModalBody><b>ANPR Point Id:</b>{" " + this.props.anprPointId}</ModalBody>
+                            
+                            <ModalBody><b>Timestamp:</b> {" " + this.props.timestamp}</ModalBody>
+                        
+                            <ModalBody><b>Vehicle Registration number:</b>{" " + this.props.vehicleRegistrationNumber}</ModalBody>
 
-    </Tab>
-    
-    <Tab eventKey="Associates" title="Associates">
-    
-    <Modal.Body class="modal-body2">
-            
-            <ModalBody><b>ANPR Point Id:</b>{" " + props.anprPointId}</ModalBody>
-            
-            <ModalBody><b>Timestamp:</b> {" " + props.timestamp}</ModalBody>
-        
-            <ModalBody><b>Vehicle Registration number:</b>{" " + props.vehicleRegistrationNumber}</ModalBody>
+                        </Modal.Body>
+                    </Tab>
+                
+                </Tabs>
+                
+                <Modal.Title></Modal.Title>
+                </Modal.Header>
 
-            
 
-            
-    </Modal.Body>
-</Tab>
-    
-    <Tab eventKey="Location" title="Location">
-    </Tab>   
-</Tabs>
-          
-          <Modal.Title></Modal.Title>
-        </Modal.Header>
-        {/* <Modal.Body>
-            <Modal.Title>Name:</Modal.Title>
-            <ModalBody>Josh</ModalBody>
-        </Modal.Body>
-        <Modal.Body>
-            <Modal.Title>Address:</Modal.Title>
-            <ModalBody>123 Fake street</ModalBody>
-        </Modal.Body>
-        <Modal.Body>
-            <Modal.Title>Place of birth: </Modal.Title>
-            <ModalBody>India</ModalBody>
-        </Modal.Body>
-        <Modal.Body>
-            <Modal.Title>Date of birth:</Modal.Title>
-            <ModalBody>03/01/1994</ModalBody>
-        </Modal.Body> */}
-        <Modal.Footer class="modal-footer">
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      </div>
-    );
-  }
+                    <Modal.Footer class="modal-footer">
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleClose}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+    );       
+
+    }
+}
