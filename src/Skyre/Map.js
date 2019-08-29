4d9b0e918fa043ccb127b682cdf1b9353cb198c8
JunichiQueen <junichim@hotmail.com>
2019-08-29 09:11:29 +0100

    maps

--- src/Skyre/Map.js
+++ src/Skyre/Map.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-export default function Map(){
-    return(
-        <div>
-            <p>Map!</p>
-        </div>
-    )
+const style = {
+    width: '100%',
+    height: '85%'
 }
+
+export class MapContainer extends Component {
+    render() {
+        return (
+            <Map
+                google={this.props.google}
+                zoom={5}
+                style={style}
+                initialCenter={{
+                    lat: 53.474420,
+                    lng: -2.286677
+                }}
+                onClick={this.onMapClicked}>
+
+                <Marker onClick={this.onMarkerClick}
+                    name={'Current location'} />
+
+                <InfoWindow onClose={this.onInfoWindowClose}>
+                    {/* <div>
+                        <h1>{this.state.selectedPlace.name}</h1>
+                    </div> */}
+                </InfoWindow>
+            </Map>
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: ("AIzaSyBt8q1SW1wHHMhOdBUnkw18M3u1LFIaPbc")
+})(MapContainer)
